3627ed3332b402492f3ff0c9d3c7301579e7386d
liza <lizokloiko@gmail.com>
2026-04-26 19:17:14 +0300

    Коммитт task_ооад_lab1

---
+++ task3_ооад_lab1/Rectangle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task3_ооад_lab1
+{
+    internal class Rectangle
+    {
+        readonly int a, b;
+        public Rectangle (int a, int b)
+        {
+            this.a = a; this.b = b;
+
+        }
+    }
+}
